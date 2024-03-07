8a5efdf7e3a23160aec09213a8e152ca97052e22
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-03-07 14:06:48 -0500

    Add password error message to css and html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// create a message below password input that says * Passwords do not match
 // while confirm password input has text in it AND the text does not equal the password text
 // make message visible
 // else make message invisible
+
+
