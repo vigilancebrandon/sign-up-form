fc1b1afd5a85778b4f6b67ab29d1280c43ade448
Vigilance <152765003+vigilancebrandon@users.noreply.github.com>
2024-03-07 13:49:40 -0500

    Start script for password confirmation

---
+++ script.js
@@ -0,0 +1,4 @@
+// create a message below password input that says * Passwords do not match
+// while confirm password input has text in it AND the text does not equal the password text
+// make message visible
+// else make message invisible
